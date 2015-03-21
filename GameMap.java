95e342f9e12274eb4c61f328b57e0a7d78f41c14
tajdim <tajdim@gmail.com>
2015-03-21 13:46:10 +0100

    Create GameMap.java

---
+++ GameMap.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class GameMap {
+
+    /**
+     * 
+     */
+    public GameMap() {
+    }
+
+    /**
+     * 
+     */
+    public ArrayList<Field> mapFields;
+
+
+
+    /**
+     * @param Point fieldPosition 
+     * @return
+     */
+    public Field getFieldAtIndex(void Point fieldPosition) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param Point fieldPosition 
+     * @param Field fieldType 
+     * @return
+     */
+    public void setFieldAtIndex(void Point fieldPosition, void Field fieldType) {
+        // TODO implement here
+        return null;
+    }
+
+}
