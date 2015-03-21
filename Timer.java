0531a4cc38bfbca5516f2e76e7033a658f265826
tajdim <tajdim@gmail.com>
2015-03-21 13:47:51 +0100

    Create Timer.java

---
+++ Timer.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Timer {
+
+    /**
+     * 
+     */
+    public Timer() {
+    }
+
+    /**
+     * @return
+     */
+    public void start() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void stop() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param String command 
+     * @return
+     */
+    public void setActionCommand(void String command) {
+        // TODO implement here
+        return null;
+    }
+
+}
