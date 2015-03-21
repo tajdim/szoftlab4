1a311983c1323af8d2c7b83d38a7e23af9c7a593
tajdim <tajdim@gmail.com>
2015-03-21 13:40:31 +0100

    Create Field.java

---
+++ Field.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Field {
+
+    /**
+     * 
+     */
+    public Field() {
+    }
+
+    /**
+     * 
+     */
+    public Point fieldPosition;
+
+    /**
+     * 
+     */
+    public final int size;
+
+    /**
+     * @param Robot robot 
+     * @return
+     */
+    public void jumpedOn(void Robot robot) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param Boolean b 
+     * @return
+     */
+    public void setChangeDirection(void Boolean b) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param Boolean b 
+     * @return
+     */
+    public void setChangeSpeed(void Boolean b) {
+        // TODO implement here
+        return null;
+    }
+
+}
