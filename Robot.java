b4a90adf55e04e0344b2b0e17171339b385cfc43
tajdim <tajdim@gmail.com>
2015-03-21 13:47:25 +0100

    Create Robot.java

---
+++ Robot.java
@@ -0,0 +1,91 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Robot {
+
+    /**
+     * 
+     */
+    public Robot() {
+    }
+
+    /**
+     * 
+     */
+    public int speed;
+
+    /**
+     * 
+     */
+    public int direction;
+
+    /**
+     * 
+     */
+    public int distance;
+
+    /**
+     * 
+     */
+    public String name;
+
+    /**
+     * 
+     */
+    public Point positionIndex;
+
+    /**
+     * 
+     */
+    public Boolean changeDirection;
+
+    /**
+     * 
+     */
+    public Boolean changeSpeed;
+
+    /**
+     * 
+     */
+    public Class fieldType;
+
+    /**
+     * 
+     */
+    public enum previousDirection;
+
+    /**
+     * 
+     */
+    public enum nextDirection;
+
+    /**
+     * @return
+     */
+    public void jump() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param Field field 
+     * @return
+     */
+    public void puttingSpot(void Field field) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param Point p 
+     * @return
+     */
+    public void setPositon(void Point p) {
+        // TODO implement here
+        return null;
+    }
+
+}
