726d866699438276877c6a2fec03aecf9161ea06
tajdim <tajdim@gmail.com>
2015-03-21 13:43:02 +0100

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Game {
+
+    /**
+     * 
+     */
+    public Game() {
+    }
+
+    /**
+     * 
+     */
+    public GameMap map;
+
+    /**
+     * 
+     */
+    public List<Robot> robots;
+
+    /**
+     * 
+     */
+    public Timer timer;
+
+
+
+
+
+
+
+    /**
+     * 
+     */
+    public GameMap 1;
+
+
+
+    /**
+     * @param String robotName 
+     * @return
+     */
+    public Point newPosition(void String robotName) {
+        // TODO implement here
+        return null;
+    }
+
+}
